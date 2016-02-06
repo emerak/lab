be4dad6031314eafea19c4abaea57582d0027d56
Alejandra <stephaniacernas@gmail.com>
2016-02-06 14:40:03 -0600

    Adds validation for date

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -14,20 +14,34 @@ class Payment < Base
   field :card_last_numbers, type: String
   field :amount, type: Integer
   field :status, type: String
+  field :status, type: String
 
   #Validations
   validates_length_of :credit_card_number, minimum: 13, maximum: 16
+  validates_length_of :expiration_month, minimum: 2, maximum: 2
+  validates_length_of :expiration_year, minimum: 2, maximum: 2
+
   validates_inclusion_of :expiration_month, in: MONTHS
-  validates_format_of :credit_card_number, with: /\A\d\z/
+
+  validates_format_of :credit_card_number, with: /\A[0-9]*\z/
+  validates_format_of :expiration_month, with: /\A\d{2}\z/
+  validates_format_of :expiration_year, with: /\A\d{2}\z/
+
   validates_presence_of :cardholder_name,
-    :expiration_month,
-    :expiration_year,
-    :card_security_code
+                        :expiration_month,
+                        :expiration_year,
+                        :card_security_code
+
+  validate :expiration_date
 
   private
 
   def bin_number=(bin_number)
     @bin_number = self.credit_card_number[0..5]
+  end
+
+  def expiration_date
+    errors.add(:expiration_year,  "Expired date")  if Time.now.strftime('%y') > expiration_year
   end
 
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -14,16 +14,38 @@ describe Payment do
     expect(payment.errors_on(:credit_card_number)).to include("is too long (maximum is 16 characters)")
   end
 
-  it "is not valid if credit_card_number has characters" do
+  it "is not valid if credit_card_number has letters" do
     payment =  build(:payment, credit_card_number: 'qoilkjlrtyuiopas')
     expect(payment).to have_at_least(1).error_on(:credit_card_number)
     expect(payment.errors_on(:credit_card_number)).to include("is invalid")
   end
 
-  it "is not valid if  expiration_month has characters" do
+  it "is not valid if  expiration_month has letters" do
     payment =  build(:payment, credit_card_number: 12345678912394, expiration_month: 'qwertyuiopas')
     payment.save
     expect(payment.errors).to have_at_least(1).error_on(:expiration_month)
     expect(payment.errors_on(:expiration_month)).to include("is not included in the list")
+  end
+
+  it "is not valid if  expiration_year has 4 length format" do
+    payment =  build(:payment, credit_card_number: 12345678912394, expiration_year: 2001)
+    payment.save
+    expect(payment.errors).to have_at_least(1).error_on(:expiration_year)
+    expect(payment.errors_on(:expiration_year)).to include("is too long (maximum is 2 characters)")
+  end
+
+  it "is not valid if  expiration_year has letters" do
+    payment =  build(:payment, credit_card_number: 12345678912394, expiration_year: '1x')
+    payment.save
+    expect(payment.errors).to have_at_least(1).error_on(:expiration_year)
+    expect(payment.errors_on(:expiration_year)).to include("is invalid")
+  end
+
+  it "is not valid if  expiration_year is minor than current year" do
+    payment =  build(:payment, credit_card_number: 12345678912394, expiration_year: '15')
+    payment.save
+    binding.pry
+    expect(payment.errors).to have_at_least(1).error_on(:expiration_year)
+    expect(payment.errors_on(:expiration_year)).to include("Expired date")
   end
 end
