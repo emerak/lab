cb45d4d85df4288903147f09d4d755fad44cdb2a
Alejandra <stephaniacernas@gmail.com>
2016-02-07 17:56:10 -0600

    Removes odd condition

--- app/models/concerns/redis_payment.rb
+++ app/models/concerns/redis_payment.rb
@@ -3,22 +3,18 @@ module RedisPayment
 
   def save_card_data
     encrypt_card
-    save_card_data
+    save_redis_data
   end
 
   private
 
   def save_redis_data
-    transaction = $redis.hmset("payments:#{_id}",
-                               'credit_card_number', encrypted_credit_card_number,
-                               'csc', encrypted_card_security_code,
-                               'expiration_month', encrypted_expiration_month,
-                               'expiration_year', encrypted_expiration_year)
+    $redis.hmset("payments:#{_id}",
+                 'credit_card_number', encrypted_credit_card_number,
+                 'csc', encrypted_card_security_code,
+                 'expiration_month', encrypted_expiration_month,
+                 'expiration_year', encrypted_expiration_year)
 
-    return if transaction
-
-    update(status: 'failed')
-    errors.add(:redis, "Redis can't save data")
   end
 
 end
